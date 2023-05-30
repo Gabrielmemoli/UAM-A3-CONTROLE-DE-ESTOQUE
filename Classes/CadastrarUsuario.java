0bbdafbb344a7247482c3fb5f53286e4e67e880a
Gustavo Martins <gustavo360@protonmail.com>
2023-05-30 20:12:32 -0300

    Classes Add

---
+++ Classes/CadastrarUsuario.java
@@ -0,0 +1,58 @@
+package Classes;
+public class CadastrarUsuario {
+    private String login;
+    private String nome;
+    private String senha;
+    private String CPF;
+    private String email;
+
+    public CadastrarUsuario(String login, String nome, String senha, String CPF, String email) {
+        this.login = login;
+        this.nome = nome;
+        this.senha = senha;
+        this.CPF = CPF;
+        this.email = email;
+    }
+
+    // Métodos getters e setters
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getCPF() {
+        return CPF;
+    }
+
+    public void setCPF(String CPF) {
+        this.CPF = CPF;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
