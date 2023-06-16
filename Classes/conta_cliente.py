b364e79a28980c295313970760a46f21fd9e5706
Lucas Sousa <AKAlucasousa@users.noreply.github.com>
2023-06-15 21:08:58 -0300

    TDD's com import do banco.db

--- Classes/conta_cliente.py
+++ Classes/conta_cliente.py
@@ -1,5 +1,7 @@
 import sys
 import os
+import sqlite3
+import unittest
 
 # Get the absolute path of the directory containing the module
 module_dir = os.path.dirname(os.path.abspath(__file__))
@@ -8,10 +10,57 @@ conta_cliente_dir = os.path.join(module_dir, 'path/to/conta_cliente')  # Replace
 # Add the module directory to the sys.path list
 sys.path.append(conta_cliente_dir)
 
-class Account:
-    def __init__(self, name, email, password, cpf, login):
-        self.name = name
-        self.email = email
-        self.password = password
-        self.cpf = cpf
-        self.login = login
+import conta_cliente
+
+class AccountTests(unittest.TestCase):
+    def setUp(self):
+        # Connect to an in-memory SQLite database
+        self.connection = sqlite3.connect(":memory:")
+        self.cursor = self.connection.cursor()
+
+        # Create a table for accounts
+        self.cursor.execute("""
+            CREATE TABLE accounts (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT,
+                email TEXT,
+                password TEXT,
+                cpf TEXT,
+                login TEXT
+            )
+        """)
+
+    def tearDown(self):
+        # Drop the accounts table and close the database connection
+        self.cursor.execute("DROP TABLE accounts")
+        self.connection.close()
+
+    def test_account_creation(self):
+        name = "Gabriel Memoli"
+        email = "gabriel@anhembi.com"
+        password = "senha123"
+        cpf = "123456789"
+        login = "gabriel"
+
+        account = conta_cliente.Account(name, email, password, cpf, login)
+
+        # Insert the account into the database
+        self.cursor.execute("""
+            INSERT INTO accounts (name, email, password, cpf, login)
+            VALUES (?, ?, ?, ?, ?)
+        """, (name, email, password, cpf, login))
+        self.connection.commit()
+
+        # Retrieve the account from the database
+        self.cursor.execute("SELECT * FROM accounts WHERE id = ?", (account.id,))
+        result = self.cursor.fetchone()
+
+        # Assert that the retrieved account matches the expected values
+        self.assertEqual(result[1], name)
+        self.assertEqual(result[2], email)
+        self.assertEqual(result[3], password)
+        self.assertEqual(result[4], cpf)
+        self.assertEqual(result[5], login)
+
+if __name__ == '__main__':
+    unittest.main()
--- Classes/test_conta_carro.py
+++ Classes/test_conta_carro.py
@@ -1,7 +1,61 @@
 import unittest
-from conta_carro import ContaCarro
+import sqlite3
+
+class ContaCarro:
+    def __init__(self, placa, modelo, marca, ano, cor, chassi, quilometragem, preco, categoria):
+        self.placa = placa
+        self.modelo = modelo
+        self.marca = marca
+        self.ano = ano
+        self.cor = cor
+        self.chassi = chassi
+        self.quilometragem = quilometragem
+        self.preco = preco
+        self.categoria = categoria
+        self.id = None
+
+    def save(self):
+        # Conecta ao banco de dados
+        connection = sqlite3.connect("banco.db")
+        cursor = connection.cursor()
+
+        # Insere o carro no banco de dados
+        cursor.execute("""
+            INSERT INTO carros (placa, modelo, marca, ano, cor, chassi, quilometragem, preco, categoria)
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+        """, (self.placa, self.modelo, self.marca, self.ano, self.cor, self.chassi, self.quilometragem, self.preco, self.categoria))
+        self.id = cursor.lastrowid
+
+        # Fecha a conexão com o banco de dados
+        connection.commit()
+        connection.close()
 
 class TestContaCarro(unittest.TestCase):
+    def setUp(self):
+        # Cria a tabela carros no banco de dados
+        self.connection = sqlite3.connect("banco.db")
+        self.cursor = self.connection.cursor()
+        self.cursor.execute("""
+            CREATE TABLE IF NOT EXISTS carros (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                placa TEXT,
+                modelo TEXT,
+                marca TEXT,
+                ano INTEGER,
+                cor TEXT,
+                chassi TEXT,
+                quilometragem INTEGER,
+                preco REAL,
+                categoria TEXT
+            )
+        """)
+
+    def tearDown(self):
+        # Apaga a tabela carros do banco de dados
+        self.cursor.execute("DROP TABLE IF EXISTS carros")
+        self.connection.commit()
+        self.connection.close()
+
     def test_criacao_conta_carro(self):
         placa = "GHYH4444"
         modelo = "FD3S"
@@ -13,18 +67,23 @@ class TestContaCarro(unittest.TestCase):
         preco = 50000000.0
         categoria = "Esportivo"
 
-
         conta_carro = ContaCarro(placa, modelo, marca, ano, cor, chassi, quilometragem, preco, categoria)
+        conta_carro.save()
 
-        self.assertEqual(conta_carro.placa, placa)
-        self.assertEqual(conta_carro.modelo, modelo)
-        self.assertEqual(conta_carro.marca, marca)
-        self.assertEqual(conta_carro.ano, ano)
-        self.assertEqual(conta_carro.cor, cor)
-        self.assertEqual(conta_carro.chassi, chassi)
-        self.assertEqual(conta_carro.quilometragem, quilometragem)
-        self.assertEqual(conta_carro.preco, preco)
-        self.assertEqual(conta_carro.categoria, categoria)
+        # Recupera o carro do banco de dados
+        self.cursor.execute("SELECT * FROM carros WHERE id = ?", (conta_carro.id,))
+        result = self.cursor.fetchone()
+
+        # Verifica se os dados do carro estão corretos
+        self.assertEqual(result[1], placa)
+        self.assertEqual(result[2], modelo)
+        self.assertEqual(result[3], marca)
+        self.assertEqual(result[4], ano)
+        self.assertEqual(result[5], cor)
+        self.assertEqual(result[6], chassi)
+        self.assertEqual(result[7], quilometragem)
+        self.assertEqual(result[8], preco)
+        self.assertEqual(result[9], categoria)
 
 if __name__ == '__main__':
     unittest.main()
